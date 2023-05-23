f050e7b1872a459103c57bd87d654a9009121f31
PC-202305051546\Administrator <quickly3@sohu.com>
2023-05-23 15:30:56 +0800

    no message

--- src/app/api/search.service.ts
+++ src/app/api/search.service.ts
@@ -14,7 +14,7 @@ export class SearchService {
   }
 
   getWordsCloud = (params: any) => {
-    return this.http.get(this.apiURL + '/infoq/getWordsCloud', { params });
+    return this.http.get(this.apiURL + '/search/getWordsCloud', { params });
   };
 
   searchDatasSimple = (params: {
@@ -22,15 +22,15 @@ export class SearchService {
     keywords: any;
     search_type: string;
   }) => {
-    return this.http.post(this.apiURL + '/infoq/getDailyList', params);
+    return this.http.post(this.apiURL + '/search/getAll', params);
   };
 
   getTags = (params: { source: string }) => {
-    return this.http.get(this.apiURL + '/infoq/getTags', { params });
+    return this.http.get(this.apiURL + '/search/getTags', { params });
   };
 
   getCategories = (params: { source: string }) => {
-    return this.http.get(this.apiURL + '/infoq/getCategories', { params });
+    return this.http.get(this.apiURL + '/search/getCategories', { params });
   };
 
   // tslint:disable-next-line: max-line-length
@@ -43,7 +43,7 @@ export class SearchService {
     endDate: NgbDateStruct | undefined;
     sortBy: { value: string; label: string };
   }) => {
-    return this.http.post(this.apiURL + '/infoq/getDailyList', params);
+    return this.http.post(this.apiURL + '/search/getAll', params);
   };
 
   getArticleHistogram = (params: {
@@ -55,33 +55,33 @@ export class SearchService {
     endDate: NgbDateStruct | undefined;
     sortBy: { value: string; label: string };
   }) => {
-    return this.http.post(this.apiURL + '/infoq/getArticleHistogram', params);
+    return this.http.post(this.apiURL + '/search/getArticleHistogram', params);
   };
 
   getWordsCloudByQueryBuilder = (params: any) => {
     return this.http.post(
-      this.apiURL + '/infoq/getWordsCloudByQueryBuilder',
+      this.apiURL + '/search/getWordsCloudByQueryBuilder',
       params
     );
   };
 
   getAuthorTermsAgg = (params: any) => {
-    return this.http.post(this.apiURL + '/infoq/getAuthorTermsAgg', params);
+    return this.http.post(this.apiURL + '/search/getAuthorTermsAgg', params);
   };
 
   getTagsTermsAgg = (params: any) => {
-    return this.http.post(this.apiURL + '/infoq/getTagsTermsAgg', params);
+    return this.http.post(this.apiURL + '/search/getTagsTermsAgg', params);
   };
 
   getCatesTermsAgg = (params: any) => {
-    return this.http.post(this.apiURL + '/infoq/getCatesTermsAgg', params);
+    return this.http.post(this.apiURL + '/search/getCatesTermsAgg', params);
   };
 
   starsChange = (params: any) => {
-    return this.http.post(this.apiURL + '/infoq/starsChange', { params });
+    return this.http.post(this.apiURL + '/search/starsChange', { params });
   };
 
   autoComplete = (params: { keywords: string }) => {
-    return this.http.get(this.apiURL + '/infoq/autoComplete', { params });
+    return this.http.get(this.apiURL + '/search/autoComplete', { params });
   };
 }
